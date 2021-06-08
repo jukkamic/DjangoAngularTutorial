d60efbfa5412c4b3cfebc917a0852c10d8c0bc25
jukkamic <jukkamic@gmail.com>
2021-06-09 02:07:43 +0300

    Add more to Employee API

--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -32,4 +32,28 @@ def departmentApi(request,id=0):
         department.delete()
         return JsonResponse("Deleted successfully!", safe=False)
 
-# Create your views here.
+@csrf_exempt
+def employeeApi(request,id=0):
+    if request.method=='GET':
+        employees = Employees.objects.all()
+        employees_serializer = EmployeeSerializer(employees, many=True)
+        return JsonResponse(employees_serializer.data, safe=False)
+    elif request.method=='POST':
+        employee_data = JSONParser().parse(request)
+        employees_serializer = EmployeeSerializer(data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Added successfully!", safe=False)
+        return JsonResponse("Failed to add.", safe=False)
+    elif request.method=='PUT':
+        employee_data = JSONParser().parse(request)
+        employee = Employees.objects.get(EmployeeId=employee_data['EmployeeId'])
+        employee_serializer = EmployeeSerializer(employee,data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Updated Successfully!", safe=False)
+
+    elif request.method=='DELETE':
+        employee=Employees.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Deleted successfully!", safe=False)
